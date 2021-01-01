38924fe7cb57c16c1bc649ce655f5cedba86fde7
Cilga Iscan <cilgaiscan@gmail.com>
2021-01-01 20:02:06 +0300

    Version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span>
+      <input type="number" v-model.number="number1" @change="onChange" />
+    </span>
+    <span>
+      <select v-model="chosenOperation" @change="onChange">
+        <option
+          :value="operation"
+          :key="operation"
+          v-for="operation in operations"
+        >
+          {{ operation }}
+        </option>
+      </select>
+    </span>
+    <span>
+      <input type="number" v-model.number="number2" @change="onChange" />
+    </span>
+    <span> = </span>
+    <span>
+      {{ result }}
+    </span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+      return {
+        number1: 3,
+        number2: 7,
+        operations: ["+", "-", "*", "/"],
+        chosenOperation: "+",
+        result: null,
+      };
+  },
+  methods: {
+    onChange() {
+      switch (this.chosenOperation) {
+        case "+":
+          this.result = this.number1 + this.number2;
+          break;
+        case "-":
+          this.result = this.number1 - this.number2;
+          break;
+        case "*":
+          this.result = this.number1 * this.number2;
+          break;
+        case "/":
+          this.result = (this.number1 / this.number2).toFixed(2);
+          break;
+        default:
+          console.log("Operation is not found!");
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input[type="number"] {
+  width: 30px;
+  height: 20px;
 }
 </style>
